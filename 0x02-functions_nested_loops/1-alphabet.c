331d908ffb7d7f91e022be936fb0f9f8fe16194b
hamza <seyedi.hamza@gmail.com>
2023-07-18 10:25:45 -0700

    r

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Description: function print the alphabet in lowercase
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
+	return (0);
+}
