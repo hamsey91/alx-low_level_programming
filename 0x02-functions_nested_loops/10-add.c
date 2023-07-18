e508636d25a42b8f10e85f465e362ba62a1d7afc
hamza <seyedi.hamza@gmail.com>
2023-07-18 12:47:12 -0700

    END

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - addition of two integers
+ *
+ * @a: takes integer type first input
+ *
+ * @b: takes integer type second input
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
+
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ *
+ * @n: the input of int type
+ */
+void print_to_98(int n)
+{
+	int c;
+
+	if (n > 98)
+	
+		for (c = n; c > 98; c--)
+			printf("%d, ", c);
+	
+	else
+		for (c = n; c < 98; c++)
+			printf("%d, ", c);
+	printf("98\n");
+}
