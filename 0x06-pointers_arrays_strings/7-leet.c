cc9dfe8d2188c37f28378aa8281d4ec3b4b9f864
hamza <seyedi.hamza@gmail.com>
2023-07-27 22:47:15 +0000

    last commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+nclude "main.h"
+/**
+ * leet - function that encodes a string into 1337.
+ *
+ * @n: input type string
+ *
+ * Return: n
+ */
+char *leet(char *n)
+{
+	int a, b;
+	char str1[] = "aAeEoOtTlL";
+	char str2[] = "4433007711";
+
+	for (a = 0; n[a] != '\0'; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (n[a] == str1[b])
+			{
+				n[a] = str2[b];
+			}
+		}
+	}
+	return (n);
+}
