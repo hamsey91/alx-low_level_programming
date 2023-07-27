ec0e27f1cc441ec5a43fbbd260033436c36b2d50
hamza <seyedi.hamza@gmail.com>
2023-07-27 04:30:20 -0700

    t

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-        int e;
+	int e;
 
-        e = 0;
-        while (*s1)
-        {
-                if (*s1 != *s2)
-                {
-                        e = ((int)*s1 - 48) - ((int)*s2 - 48);
-                        break;
-                }
-                s1++;
-                s2++;
-        }
-        return (e);
+	e = 0;
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			e = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (e);
 }
