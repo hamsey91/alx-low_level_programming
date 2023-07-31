1998831606d6c96201ee91b79b38c0cf499ec989
hamza <seyedi.hamza@gmail.com>
2023-07-31 15:51:39 +0000

    last commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,7 +14,7 @@ unsigned int _strspn(char *s, char *accept)
 
 		for (a = 0; s[a] != '\0'; a++)
 		{
-			for (b = 0; accept[b] != '\0'; b++)
+			for (b = 0; accept[b] != s[a]; b++)
 			{
 				if (accept[b] == '\0')
 					return (a);
