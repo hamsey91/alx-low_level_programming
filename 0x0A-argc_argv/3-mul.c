1608515b1c6873406f81a231749cdc6d5024e186
hamza <seyedi.hamza@gmail.com>
2023-08-05 00:04:54 +0000

    argc,argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - print the result of the multiplication
+ *
+ *@argc: number of argument
+ *@argv: pointer to array 
+ *
+ *Return: 0 (Success), 1 (Error)
+ */
+int main(int argc,char *argv[])
+{
+	int result;
+
+	if (argc == 3)
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
