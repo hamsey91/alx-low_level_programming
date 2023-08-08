fdc4e885135f9905988efadb05c80c1445d6b382
hamza <seyedi.hamza@gmail.com>
2023-08-08 22:44:56 +0000

    end commit

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,11 +10,11 @@
  *Return: void
  */
 
-void free_grid(int **grid, int height)		
+void free_grid(int **grid, int height)
 {
 	int i;
 
-	for (i = 0; i < height; i++)	
+	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
