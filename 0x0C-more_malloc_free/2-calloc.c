533e4a0cf4f9b71fc4838d801dd26cbbef658073
hamza <seyedi.hamza@gmail.com>
2023-08-11 01:01:01 +0000

    last commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -31,15 +31,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *m;
 
-	if (nmemb == 0 || size == 0)
+	if (size == 0 || nmemb == 0)
 		return (NULL);
-
 	m = malloc(sizeof(int) * nmemb);
 
-	if (m == NULL)
+	if (m == 0)
 		return (NULL);
 
-	_memset(m, 0, nmemb * sizeof(int));
+	_memset(m, 0, sizeof(int) * nmemb);
 
 	return (m);
 }
