2daf81c7ca9a75b925ee2b190c2672c3716a07ce
hamza <seyedi.hamza@gmail.com>
2023-08-14 23:26:33 +0000

    last commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,13 +4,15 @@
 /**
  * print_dog - prints a struct dog
  * @d: dog to print
+ *
+ * Return: void
  */
 void print_dog(struct dog *d)
 {
 	if (d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
-		printf("Age: %s\n", d->age));
+		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
