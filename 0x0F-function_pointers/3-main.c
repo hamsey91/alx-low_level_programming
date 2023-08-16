a09b06fa38467e309300309f7f164bc76ee81b38
hamza <seyedi.hamza@gmail.com>
2023-08-16 23:05:00 +0000

    last commit

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+/**
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int na, nb;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	na = atoi(argv[1]);
+	op = argv[2];
+	nb = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && nb == 0) ||
+	    (*op == '%' && nb == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(na, nb));
+
+	return (0);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: The first int
+ * @b: The second int
+ *
+ * Return: The sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts two integers
+ * @a: The first int
+ * @b: The second int
+ *
+ * Return: The difference.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two integers
+ * @a: The first int
+ * @b: The second int
+ *
+ * Return: The product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two integers
+ * @a: The first int
+ * @b: The second int
+ *
+ * Return: The quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - the modulo of two integers
+ * @a: The first int
+ * @b: The second int
+ *
+ * Return: The remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
