3c8841eb4edaa022c1322ae69cdc91f1126e0ad7
hamza <seyedi.hamza@gmail.com>
2023-08-26 00:46:14 +0000

    last commit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - function that returns the number of elements in a linked list_t list.
+ * list_len - function that returns the number of elements in a linked list_t
  *
  * @h: pointer to the first node
  *
@@ -9,7 +9,7 @@
  */
 size_t list_len(const list_t *h)
 {
- 	size_t n_count = 0;
+	size_t n_count = 0;
 
 	while (h)
 	{
