c585d803b0f1eb8e295ef535dba4d1b063e60b3a
hamza <seyedi.hamza@gmail.com>
2023-08-26 00:33:42 +0000

    last commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,24 +10,21 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	unsigned int len = 0
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
+	unsigned int len = 0;
 
-	if (!head || !new_node)
+	while (str[len])
+	len++;
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
 		return (NULL);
-	if (str)
-	{
-		new_node->str = strdup(str);
-		if (!new_node->str)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		new_node->len = len;
-	}
 
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+	new_node->str = strdup(str);
+	new_node->len = len;
+	new_node->next = (*head);
+	(*head) = new_node;
+
+	return (*head);
 }
 
