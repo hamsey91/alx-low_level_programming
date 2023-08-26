a1a6ddcd117a9619cfdf8940a5c9ab0381fb817a
hamza <seyedi.hamza@gmail.com>
2023-08-26 00:43:36 +0000

    last commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,17 @@
 
 size_t print_list(const list_t *h)
 {
-        size_t i = 0;
+	size_t i = 0;
 
-        while (h)
-        {
-                if (h->str == NULL)
-                        printf("[0] (nil)\n");
-                else
-                        printf("[%d] %s\n", h->len, h->str);
-                h = h->next;
-                i++;
-        }
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
 
-        return (i);
+	return (i);
 }
