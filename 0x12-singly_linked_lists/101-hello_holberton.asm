3069157f2263310c6b1795c136d94a0c3eb74f17
hamza <seyedi.hamza@gmail.com>
2023-08-26 01:16:38 +0000

    last commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,11 @@
-section 	.text
-	global    main
-	extern    printf
+section .text
+	global	main
+	extern	printf
 main:
-	mov		edi, format
-	mov		eax, 0
-	call		printf
-section		.data
+	mov	edi, format
+	mov	eax, 0
+	call	printf
+
+section	.data
 	format db 'Hello, Holberton', 0xa, 0
 
