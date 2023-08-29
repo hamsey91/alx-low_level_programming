1a397ccb6ce91f0a21d6c7fc244a9f816f31c4b6
hamza <seyedi.hamza@gmail.com>
2023-08-29 15:41:06 +0000

    mandatory tasks end

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -4,7 +4,7 @@
  * delete_nodeint_at_index - unction that deletes the node at index index
  *			of a listint_t linked list.
  *
- * @head: pointer to the first node
+ * @head: pointer to pointer to the first node
  * @index: the index of the node that should be deleted.
  *
  * Return: 1 if it succeeded, -1 if it failed
