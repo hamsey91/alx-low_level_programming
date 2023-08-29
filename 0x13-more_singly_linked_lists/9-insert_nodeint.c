feffd909a6c8ef70cc5a9bc8f7f7e4ba5cd8e675
hamza <seyedi.hamza@gmail.com>
2023-08-29 15:13:36 +0000

    mandatory tasks end

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	listint_t *node, *new_node = malloc(sizeof(listint_t));
 
 	if (!new_node || !head)
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	for (i = 0; node && i < idx; i++)
+	for (node = *head; node && i < idx; i++)
 	{
 		if (i == idx - 1)
 		{
