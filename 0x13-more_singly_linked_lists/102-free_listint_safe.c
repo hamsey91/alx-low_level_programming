ce44595fb12d0b5c396573878e436e8acb21dd42
hamza <seyedi.hamza@gmail.com>
2023-08-30 00:34:57 +0000

    optional tasks

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,4 +1,30 @@
 #include "lists.h"
+#include <stdlib.h>
+
+/**
+ * f_loop_place - function that find a loop in a a listint_t list.
+ *
+ * @head: pointer to pointer to the first node
+ *
+ * Return: adress to the node where the loop begin, 0 if no loop
+ */
+listint_t *f_loop_place(listint_t *head)
+{
+	listint_t *p, *end;
+
+	if (!head)
+		return (NULL);
+
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (p = head; p != end; p = p->next)
+			if (p == end->nest)
+				return (end->next);
+	}
+	return (NULL);
+}
 
 /**
  * free_listint_safe - function that frees a listint_t list.
@@ -9,30 +35,31 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *node;
+	listint_t *next, *lp_node;
 	size_t len;
-	int loop;
+	int loop = 1;
 
-	if (h == NULL || *h == NULL)
+	if (!h || !*h)
 		return (0);
 
-	while (*h)
+	lp_nood = f_loop_place(*h);
+	for (len = 0; (*h != loopnode || loop) && *h != NULL; *h = next)
 	{
-		loop = *h - (*h)->next;
-		if (loop > 0)
+		len++;
+		next = (*h)->next;
+		if (*h == lp_node && loop)
 		{
-			node = (*h)->next;
-			free(*h);
-			*h = node;
+			if (lp_node == lp_node->next)
+			{
+				free(*h);
+				break;
+			}
 			len++;
+			next = next->next;
+			free((*h)->next);
+			loop = 0;
 		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-			len++;
-			break;
-		}
+		free(*h);
 	}
 
 	*h = NULL;
