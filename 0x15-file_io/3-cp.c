1b0af0689285f9bada748fa3e27bf877195df322
hamza <seyedi.hamza@gmail.com>
2023-09-04 23:36:27 +0000

    file i/o tasks

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 #define USAGE "Usage: cp file_from file_to\n"
-#define NOT_READ_ERROR "Error: Can't read from file %s\n"
-#define NOT_WRITE_ERROR "Error: Can't read from file %s\n"
-#define NOT_CLOSE_ERROR "Error: Can't close fd %d\n"
+#define READ_ERROR "Error: Can't read from file %s\n"
+#define WRITE_ERROR "Error: Can't write to %s\n"
+#define CLOSE_ERROR "Error: Can't close fd %d\n"
 #define PERMISSIONS "(S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
@@ -24,23 +24,23 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
-		dprintf(STDERR_FILENO, NOT_READ_ERROR, argv[1]), exit(98);
+		dprintf(STDERR_FILENO, READ_ERROR, argv[1]), exit(98);
 	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (to == -1)
-		 dprintf(STDERR_FILENO, NOT_WRITE_ERROR, argv[2]), exit(99);
+		 dprintf(STDERR_FILENO, WRITE_ERROR, argv[2]), exit(99);
 
 	while (( b = read(from, buffer, READ_BUF_SIZE)) > 0)
 		if (write(to, buffer, b) != b)
-			 dprintf(STDERR_FILENO, NOT_WRITE_ERROR, argv[2]), exit(99);
+			 dprintf(STDERR_FILENO, WRITE_ERROR, argv[2]), exit(99);
 	if (b == -1)
-		 dprintf(STDERR_FILENO, NOT_READ_ERROR, argv[1]), exit(98);
+		 dprintf(STDERR_FILENO, READ_ERROR, argv[1]), exit(98);
 
 	from = close(from);
 	to = close(to);
 	if (from)
-		 dprintf(STDERR_FILENO, NOT_CLOSE_ERROR, from), exit(100);
+		 dprintf(STDERR_FILENO, CLOSE_ERROR, from), exit(100);
 	if (to)
-		 dprintf(STDERR_FILENO, NOT_CLOSE_ERROR, from), exit(100);
+		 dprintf(STDERR_FILENO, CLOSE_ERROR, from), exit(100);
 
 	return (EXIT_SUCCESS);
 }
