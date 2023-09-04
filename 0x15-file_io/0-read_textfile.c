a0dac7fcf9f6c1d056eceb16bd2ff3c383fbbd42
hamza <seyedi.hamza@gmail.com>
2023-09-04 17:47:57 +0000

    file i/o tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,7 @@
  * read_textfile- function that reads a text file and prints it to
  *		the POSIX standard output.
  *
- * @filename: file name to be read 
+ * @filename: file name to be read
  * @letters: the number of letters it should read and print
  *
  * Return: the actual number of letters it could read and print
@@ -19,8 +19,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	if(fd == -1)
-		return(0);
+	if (fd == -1)
+		return (0);
 
 	b = read(fd, &buf[0], letters);
 	b = write(STDOUT_FILENO, &buf[0], b);
