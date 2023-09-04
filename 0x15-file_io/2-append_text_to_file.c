c2c33386f4fde9bf6aa0d17653f8e0310c73146f
hamza <seyedi.hamza@gmail.com>
2023-09-04 22:32:32 +0000

    file i/o tasks

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,21 +8,21 @@
  *
  * Return: 1 on success and -1 on failure
  */
-int append_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content);
 {
 	int a, b;
 	size_t len = 0;
 
 	if (!filename)
-		return (-1);
+	return (-1);
 
 	while (text_content && text_content[len] != '\0')
 	{
-		len++;
+	len++;
 	}
 
-	a = open(filename, O_WRONLY | O_APPEND);
-	b = write(o, text_content, len);
+	a = open(filename, O_WRONGLY | O_APPEND);
+	b = write(a, text_content, len);
 
 	if (a == -1 || b == -1)
 		return (-1);
@@ -31,4 +31,3 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	return (1);
 }
-
