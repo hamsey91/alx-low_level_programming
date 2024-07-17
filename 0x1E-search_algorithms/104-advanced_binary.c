035a07be2ef0b2f7cf59a3afffaa2bd6dde03695
hamza <seyedi.hamza@gmail.com>
2024-07-17 14:53:52 -0700

    advanced tasks

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,34 +1,36 @@
 #include "search_algos.h"
 
 /**
-  * recursive_binary_search - Function that searches for a value in a sorted
-  *		array of integers using a recursive banary search.
+  * recursive_binary - Function that searches for a value in a sorted array
+  *		of integers using a recursive banary search
   * @array: Pointer to the first element of the sub-array.
-  * @gauche: The first index of the sub-array.
-  * @droite: The last index of the sub-array.
+  * @gauche: The first index of the subarray.
+  * @droite: The last index of the subarray.
   * @value: The value to search for it.
   *
   * Return: -1 If the value is not present or the array is NULL
-  *	The index where the value is located.
+  *			The index where the value is located.
+  *
+  * Description: Prints the subarray being searched after each change.
   */
-int recursive_binary(int *array, size_t gauche, size_t droite, int value)
+int rec_binary(int *array, size_t gauche, size_t droite, int value)
 {
-	size_t idx;
+	size_t i;
 
 	if (droite < gauche)
 		return (-1);
 
 	printf("Searching in array: ");
-	for (idx = gauche; idx < droite; idx++)
-		printf("%d, ", array[idx]);
-	printf("%d\n", array[idx]);
+	for (i = gauche; i < droite; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
 
-	idx = gauche + (droite - gauche) / 2;
-	if (array[idx] == value && (idx == gauche || array[idx - 1] != value))
-		return (idx);
-	if (array[idx] >= value)
-		return (recursive_binary(array, gauche, idx, value));
-	return (recursive_binary(array, idx + 1, droite, value));
+	i = gauche + (droite - gauche) / 2;
+	if (array[i] == value && (i == gauche || array[i - 1] != value))
+		return (i);
+	if (array[i] >= value)
+		return (rec_binary(array, gauche, i, value));
+	return (rec_binary(array, i + 1, droite, value));
 }
 
 /**
@@ -38,14 +40,16 @@ int recursive_binary(int *array, size_t gauche, size_t droite, int value)
   * @size: The size of the array.
   * @value: The value to search for it.
   *
-  * Return: -1 If the value is not present or the array is NULL
-  *	The index where the value is located.
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the first index where the value is located.
+  *
+  * Description: Prints the subarray being searched after each change.
   */
 int advanced_binary(int *array, size_t size, int value)
 {
 	if (array == NULL || size == 0)
 		return (-1);
 
-	return (recursive_binary(array, 0, size - 1, value));
+	return (rec_binary(array, 0, size - 1, value));
 }
 
